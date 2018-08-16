1d5e83234163f90f67a6516ca9663316c8e14bf6
David Kubec <davesade.42@gmail.com>
2018-08-16 10:28:51 +0200

    Added encryption also to log bucket.

--- terraform/listme_ops.tf
+++ terraform/listme_ops.tf
@@ -1,17 +1,27 @@
-resource "aws_s3_bucket" "listmelogs" {
-  bucket = "vvc.listme.logs"
-  acl    = "log-delivery-write"
-
-  versioning {
-    enabled = false
-  }
-}
-
 resource "aws_kms_key" "listmeops" {
   description             = "This key is used to encrypt bucket objects"
   deletion_window_in_days = 10
 }
 
+resource "aws_kms_key" "listmelogs" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket" "listmelogs" {
+  depends_on = ["aws_kms_key.listmelogs"]
+  bucket = "vvc.listme.logs"
+  acl    = "log-delivery-write"
+  
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.listmlogs.arn}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
 
 resource "aws_s3_bucket" "listmeops" {
   depends_on = ["aws_s3_bucket.listmelogs", "aws_kms_key.listmeops"]
