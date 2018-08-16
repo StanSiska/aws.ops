ac05bd6ce5057fa19d19f37c8fbe15436c473045
David Kubec <davesade.42@gmail.com>
2018-08-16 10:30:22 +0200

    Fixed name of refernce object.

--- terraform/listme_ops.tf
+++ terraform/listme_ops.tf
@@ -16,7 +16,7 @@ resource "aws_s3_bucket" "listmelogs" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = "${aws_kms_key.listmlogs.arn}"
+        kms_master_key_id = "${aws_kms_key.listmelogs.arn}"
         sse_algorithm     = "aws:kms"
       }
     }
