bb7cc98ee750cb6911d820a47451989b247aeba2
David Kubec <davesade.42@gmail.com>
2018-08-16 10:26:06 +0200

    Added KMS encryption to ops bucket.

--- terraform/listme_ops.tf
+++ terraform/listme_ops.tf
@@ -7,9 +7,25 @@ resource "aws_s3_bucket" "listmelogs" {
   }
 }
 
+resource "aws_kms_key" "listmeops" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+
 resource "aws_s3_bucket" "listmeops" {
-  depends_on = ["aws_s3_bucket.listmelogs"]
+  depends_on = ["aws_s3_bucket.listmelogs", "aws_kms_key.listmeops"]
   bucket = "vvc.listme.ops"
+  
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.listmeops.arn}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+  
   acl    = "private"
 
   versioning {
